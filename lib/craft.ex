87804197212a93050e19534a24acc73bd783b699
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-04-28 15:54:57 +0200

    Initiate the new mix project

---
+++ lib/craft.ex
@@ -0,0 +1,18 @@
+defmodule Craft do
+  @moduledoc """
+  Documentation for Craft.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Craft.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/craft_test.exs
@@ -0,0 +1,8 @@
+defmodule CraftTest do
+  use ExUnit.Case
+  doctest Craft
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
