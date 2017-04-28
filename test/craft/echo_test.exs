6504304fbcbe35ac775e6a917450c78043364bc3
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-04-28 16:53:30 +0200

    Add a test for handling different messages and time outs

--- test/craft/echo_test.exs
+++ test/craft/echo_test.exs
@@ -1,10 +1,23 @@
-
 defmodule Craft.EchoTest do
   use ExUnit.Case, async: true
+  alias Craft.Echo
 
   test "echo" do
     {:ok, pid} = Echo.start_link()
     Echo.send(pid, :hello)
     assert_receive :hello
+    Echo.send(pid, :hello)
+    assert_receive :hello
+    Echo.send(pid, :hello)
+    assert_receive :hello
+
+    send(pid, :another_message)
+    assert Process.alive?(pid )
+  end
+
+  test "times out after 50 ms" do
+    {:ok, pid} = Echo.start_link()
+    Process.sleep(51)
+    refute Process.alive?(pid )
   end
 end
